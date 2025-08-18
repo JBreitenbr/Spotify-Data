b97f62a6da471abc3129d2854604edeea7d81b53
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-08-18 17:45:13 +0000

    new data

--- sub.py
+++ sub.py
@@ -1,5 +1,5 @@
 import pandas as pd 
-df0=pd.read_csv('spotiData_01Aug2025.csv')
+df0=pd.read_csv('spotiData_18Aug2025.csv')
 cnt=df0["artist"].value_counts()
 df=pd.merge(df0,cnt,on="artist")
 print(len(df))
